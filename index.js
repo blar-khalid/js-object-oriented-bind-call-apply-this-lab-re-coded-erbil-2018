9f92c527de38481a0f78099461ccc982cfca11d0
blar khalid <blarharki@gmail.com>
2018-09-05 10:06:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function justInvoke(func) {
   return func();
 }
+function setThisWithCall(func, objectt, arg) {
+  return func.call(objectt, arg);
+}
+
+function setThisWithApply(func,objectt, arg) {
+  return func.apply(objectt,arg);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  const copy = functionToBeCopied.bind(thisValue);
+  return copy;
+}
